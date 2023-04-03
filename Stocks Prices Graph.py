fa838d493d3cf34b430d0303bd80919bc62181ec
Kenshi Sanada <72691765+khrusenada@users.noreply.github.com>
2023-04-03 16:13:29 +0700

    Add files via upload

---
+++ Stocks Prices Graph.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import date, timedelta
+
+stocks = input("Enter stock symbol: ")
+today = date.today()
+
+d1 = today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today() - timedelta(days=1080)
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
+
+data = yf.download(stocks, 
+                      start=start_date, 
+                      end=end_date, 
+                      progress=False)
+
+
+#change date from column to index
+data["Date"] = data.index
+
+data = data[["Date", "Open", "High", 
+             "Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+
+
+# Convert string to datetime64
+data['Date'] = data['Date'].apply(pd.to_datetime)
+
+#to make sure date colum is in index column
+data.set_index('Date', inplace=True)
+
+
+data['Adj Close'].plot(figsize=(16,8),title='Adjusted Closing Price')
+
+plt.show()
+
